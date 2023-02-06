f2a59ca82af48b2c4b8909add60719688a445791
Mehrdad <mehrdad.zeinali@epitech.eu>
2023-02-06 01:19:15 +0100

    [UPDATE] error controls in english

--- vacuum_pos.py
+++ vacuum_pos.py
@@ -41,19 +41,18 @@ while True:
     try:
         grid_size = tuple(map(int, input().split()))
         if len(grid_size) != 2:
-            raise ValueError("La première ligne doit contenir 2 chiffres.")
+            raise ValueError("The first line must contain 2 digits with a space between them.")
         val = input().split()
         position = tuple(map(int, val[:2]))
         orientation = val[2]
         if len(position) != 2 or len(orientation) != 1 or orientation not in ['N', 'E', 'S', 'W']:
-            raise ValueError("La deuxième ligne doit contenir 2 chiffres et une orientation en majuscule (N, E, S ou W).")
+            raise ValueError("The second line must contain 2 numbers and an uppercase orientation (N, E, S or W).")
         commands = input().strip()
         if not all(c in ['A', 'D', 'G'] for c in commands):
-            raise ValueError("La troisième ligne doit contenir uniquement des 'A', 'D' et 'G'.")
+            raise ValueError("The third line must contain only 'A', 'D' and 'G'.")
         final_position, final_orientation = run_commands(commands, position, orientation, grid_size)
         print(" ".join(map(str, final_position)) + " " + final_orientation)
         break
     except Exception as e:
         print(e)
         continue
-
